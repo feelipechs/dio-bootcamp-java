cfb9ee2023957d034790021811237d0e8131725c
feelipechs <flpchagas@outlook.com>
2024-05-04 18:13:23 -0300

    Atualizando

--- dio-java-basico/tipos-variaveis/src/TiposVariaveis.java
+++ dio-java-basico/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        // estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Felipe Chagas";
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(meuNome + salarioMinimo + numeroCurto2 + numero + VALOR_DE_PI);
     }
 }
