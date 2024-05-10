747df30dd04a8816876b3d6ea90ec67f0b25c7c5
feelipechs <flpchagas@outlook.com>
2024-05-10 12:24:43 -0300

    Atualizando arquivos

---
+++ dio-java-basico/controle-fluxo/src/ExemploFormatadorCep.java
@@ -0,0 +1,15 @@
+// public class ExemploFormatadorCep {
+//     public static void main(String[] args) {
+//         String CepFormatado = formatarCep("23765064");
+//     }
+// }
+
+// public class CepInvalidoException extends Exception {}
+
+// static String formatarCep(String cep) throws CepInvalidoException{
+//     if(cep.length() != 8)
+//       throw new CepInvalidoException();
+    
+//       //simulando um cep formatado
+//       return "23.765-064";
+// }
