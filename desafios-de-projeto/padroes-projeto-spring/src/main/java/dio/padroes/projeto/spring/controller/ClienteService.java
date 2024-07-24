ce81e6b0914f9107cdd37a942e420452b06c81d8
feelipechs <flpchagas@outlook.com>
2024-07-24 14:47:26 -0300

    atualizando

---
+++ desafios-de-projeto/padroes-projeto-spring/src/main/java/dio/padroes/projeto/spring/controller/ClienteService.java
@@ -0,0 +1,32 @@
+package dio.padroes.projeto.spring.controller;
+
+import dio.padroes.projeto.spring.model.Cliente;
+
+public class ClienteService {
+
+    public Object buscarTodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarTodos'");
+    }
+
+    public Object buscarPorId(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarPorId'");
+    }
+
+    public void inserir(Cliente cliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'inserir'");
+    }
+
+    public void atualizar(Long id, Cliente cliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
+    }
+
+    public void deletar(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletar'");
+    }
+
+}
---
+++ desafios-de-projeto/padroes-projeto-spring/src/test/java/dio/padroes/projeto/spring/PadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.padroes.projeto.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
